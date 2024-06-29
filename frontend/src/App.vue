70a59178de0d8bbe385b30e8582a2a457fbdc84e
saul dominguez <dguezs@gmail.com>
2024-06-29 01:53:27 -0600

    ajustes en vista home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -15,26 +15,12 @@
           width="40"
         />
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <span class="mr-10">API Usuarios</span>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      
     </v-app-bar>
 
     <v-main>
